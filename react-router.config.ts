70343f8b62f2c9a48733f6c2ec4bc9725ff794a9
heiwa4126 <heiwa4126@gmail.com>
2025-04-17 17:20:25 +0900

    update

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,23 @@
 import type { Config } from "@react-router/dev/config";
 
+const pages = ["/", "/dataloader", "/action"];
+
 export default {
 	// Config options...
 	// Server-side render by default, to enable SPA mode set this to `false`
-	ssr: true,
+
+	// パターン 0 : `pnpm build && pnpm start`
+	// ssr: true,
+
+	// パターン 1 : `pnpm build && pnpm start`
+	// ssr: true,
+	// async prerender() {
+	// 	return pages;
+	// },
+
+	// パターン 2 : `pnpm build && pnpm preview`
+	ssr: false,
+	async prerender() {
+		return pages;
+	},
 } satisfies Config;
